42ad267ec2a38200e714b04f905f72f6fecf5d17
Jérôme Boisseau <jerome.boisseau@outlook.com>
2017-03-31 17:00:34 +0200

    Passsage sass

--- YourIS/gulpfile.js
+++ YourIS/gulpfile.js
@@ -4,7 +4,19 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var sass = require("gulp-sass");
+
+//where to find sass code
+paths.sassSource = paths.webroot + "css/**/*.scss";
+//where to output compiled CSS code
+paths.cssOutput = paths.webroot + "css";
 
 gulp.task('default', function () {
     // place code for your default task here
+});
+
+gulp.task('sass', function () {
+    gulp.src(paths.sassSource)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.cssOutput));
 });
